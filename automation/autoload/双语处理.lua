9863824f3d39c4cf9d5ac868cfc1e85ce94677d0
presisco <internight@sina.com>
2017-01-06 13:59:01 +0800

    将“双语后处理”与“双语预处理”合并为双语处理
    有待测试

---
+++ automation/autoload/双语处理.lua
@@ -0,0 +1,226 @@
+﻿--[[
+Copyright (c) 2017 Presisco
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial 
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES 
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
+require "bakery-filter-style-layer"
+require "bakery-basic-ui"
+require "bakery-utils"
+require "bakery-locale"
+unicode = require 'aegisub.unicode'
+
+local tr = aegisub.gettext
+
+script_name = tr"双语处理"
+script_description = tr"对字母进行双语处理（两行形式），目前只支持英语和非英语"
+script_author = "presisco"
+script_version = "2.00"
+script_modified = "1 January 2017"
+line_end = "\\N"
+left_brace = 123
+right_brace = 125
+ignored_symbols_seperator = ","
+
+subs={}
+selections={}
+active={}
+available_styles={}
+export_config={}
+
+function find_last_pos(line,judger)
+	local reading_tags=false
+	local last_pos=0
+	local i=0
+	
+	for char in unicode.chars(line)
+	do
+		local char_length=char:len()
+		i=i+char_length
+		if char == left_brace
+		then
+			reading_tags=true
+		elseif char == right_brace
+		then
+			reading_tags=false
+		elseif not reading_tags
+		then
+			if judger(char)
+			then
+				last_pos=i
+			end
+		end
+	end
+	
+	return last_pos
+end
+
+function reassgin_line(line,last_pos,linebreaker)
+	local top_text=""
+	local bottom_text=""
+	
+	divide_start,divide_end=line:find(linebreaker,last_pos,true)
+	
+	if last_pos == 0
+	then
+		bottom_text=line:gsub(linebreaker,"")
+	elseif divide_start ~= nil
+	then
+		top_text=line:sub(1,divide_start-1)
+		top_text=top_text:gsub(linebreaker,"")
+		if divide_end < line:len()
+		then
+			bottom_text=line:sub(divide_end+1,line:len())
+			bottom_text=bottom_text:gsub(linebreaker," ")
+		end
+	else
+		top_text=line:gsub(linebreaker,"")
+	end
+	
+	return top_text,bottom_text
+end
+
+function left_overs(subtitle,top_text,bottom_text,result)
+	
+	if result.drop_top_tags 
+	then
+		top_text=top_text:gsub("{[^}]+}", "")
+	end
+	
+	if result.drop_bottom_tags 
+	then
+		bottom_text=bottom_text:gsub("{[^}]+}", "")
+	end
+	
+	local top_complete=""
+	local bottom_complete=""
+	local divider=""
+	local new_text=""
+	
+	if result.cook_empty 
+	then
+		top_complete=result.top_tags..top_text
+		bottom_complete=result.bottom_tags..bottom_text
+		divider=result.linebreaker
+	else
+		if top_text ~= ""
+		then
+			top_complete=result.top_tags..top_text
+		end
+		if bottom_text ~= ""
+		then
+			bottom_complete=result.bottom_tags..bottom_text
+		end
+		if bottom_text ~= "" and top_text ~= ""
+		then
+			divider=result.linebreaker
+		end
+	end	
+	
+	if result.switch_lines
+	then
+		new_text=bottom_complete..divider..top_complete
+	else
+		new_text=top_complete..divider..bottom_complete
+	end
+	
+	local new_line = subtitle
+	
+	if result.alter_style
+	then
+		new_line.style = result.new_style
+	end
+	
+	if result.alter_layer
+	then
+		new_line.layer = result.new_layer
+	end
+	
+	new_line.text = new_text
+	return new_line
+end
+
+function scan_subs(result)
+	for i = 1, #subs
+	do
+		aegisub.progress.set(i * 100 / #subs)
+		if bakery_filter_style_layer(subs[i],result)
+		then
+			last_pos=find_last_pos(subs[i].text,bakery_get_language_judger_by_name(result.top_language))
+			top_text,bottom_text=reassgin_line(subs[i].text,last_pos,result.linebreaker)
+			subs[i] = left_overs(subs[i],top_text,bottom_text,result)
+		end
+	end
+end
+
+function on_ok(result)
+	aegisub.log("picked language:"..result.top_language.."\n")
+	aegisub.log("linebreaker:"..result.linebreaker.."\n")
+	scan_subs(result)
+	aegisub.set_undo_point(script_name)
+end
+
+function on_cancel(result)
+	aegisub.cancel()
+end
+
+function entry()
+	available_styles=bakery_get_available_style_names(subs)
+	available_languages=bakery_get_language_names()
+	
+	local dialog_config = {
+		{class="label",label="上层要添加的标签",width=10,height=1},
+		{class="textbox",name="top_tags",hint="tags",width=10,height=3},
+		{class="label",label="下层要添加的标签",width=10,height=1},
+		{class="textbox",name="bottom_tags",hint="tags",width=10,height=3},
+		{class="label",label="上层原先的主要语言",width=8,height=1},
+		{class="dropdown",name="top_language",hint="language",items=available_languages,value=available_languages[1],width=8,height=1},
+		{class="label",label="换行符",width=4,height=1},
+		{class="textbox",name="linebreaker",hint="characters",value="\\N",width=8,height=1},
+		{class="checkbox",label="更改风格",name="alter_style",width=4,height=1},
+		{class="dropdown",name="new_style",hint="style name",items=available_styles,value=available_styles[1],width=8,height=1},
+		{class="checkbox",label="更改图层",name="alter_layer",width=5,height=1},
+		{class="intedit",name="new_layer",hint="layer id",width=2,height=1,min=0},
+		{class="checkbox",label="丢弃翻译的标签",name="drop_top_tags",width=8,height=1},
+		{class="checkbox",label="丢弃原文的标签",name="drop_bottom_tags",width=8,height=1},
+		{class="checkbox",label="交换输出行",name="switch_lines",width=5,height=1},
+		{class="checkbox",label="空行添加标签和换行",name="cook_empty",value=true,width=10,height=1}
+	}
+	
+	bakery_compute_layout_vertical(dialog_config,0,0)
+	
+	bakery_merge_table(dialog_config
+			,bakery_get_filter_style_layer_ui_vertical(available_styles,0
+				,bakery_get_total_height(dialog_config)))
+			
+	bakery_simple_dialog_ok_cancel(dialog_config,on_ok,on_cancel)
+	
+end
+
+function bilingual_cook_macro(subtitles, selected_lines, active_line)
+	subs=subtitles
+	selections=selected_lines
+	active=active_line
+	entry()
+end
+
+function bilingual_cook_filter(subtitles, config)
+	subs=subtitles
+	export_config=config
+	entry()
+end
+
+aegisub.register_macro(script_name, script_description, bilingual_cook_macro)
+aegisub.register_filter(script_name, script_description, 0, bilingual_cook_filter)
---
+++ automation/autoload/双语模板操作.lua
@@ -0,0 +1,117 @@
+﻿--[[
+Copyright (c) 2017 Presisco
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial 
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES 
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
+require "bakery-filter-style-layer"
+require "bakery-basic-ui"
+require "bakery-utils"
+
+local tr = aegisub.gettext
+
+script_name = tr"双语模板操作"
+script_description = tr"对字幕进行模板操作，包括双语预处理，后处理等"
+script_author = "presisco"
+script_version = "1.00"
+script_modified = "3 January 2017"
+
+dialog_mode={
+	{class="label",label="选择模式",x=0,y=0,width=5,height=1},
+	{class="dropdown",name="mode",items={"生成模板","套用模板"},value="套用模板",x=0,y=1,width=5,height=1}
+}
+
+dialog_export_config={
+	{class="label",label="风格",x=0,y=0,width=20,height=1},
+	{class="textbox",name="style",hint="style",x=0,y=1,width=8,height=1,value=""},
+	{class="label",label="图层",x=0,y=0,width=20,height=1},
+	{class="intedit",name="layer",hint="layer",x=0,y=1,width=8,height=1,value=0,min=0},
+	{class="label",label="文本",x=0,y=0,width=20,height=1},
+	{class="textbox",name="text",hint="text",x=0,y=1,width=20,height=3,value=""},
+	{class="label",label="第一行的效果",x=0,y=4,width=20,height=1},
+	{class="textbox",name="trans_prefix",hint="tags",x=0,y=5,width=20,height=3,value=""},
+	{class="label",label="第二行的效果",x=0,y=8,width=20,height=1},
+	{class="textbox",name="src_prefix",hint="tags",x=0,y=9,width=20,height=3,value=""},
+	{class="label",label="分隔符",x=0,y=12,width=20,height=1},
+	{class="textbox",name="seperator",hint="seperator",x=0,y=13,width=5,height=1,value=""},
+	{class="label",label="模板名称",x=0,y=14,width=20,height=1},
+	{class="textbox",name="tpl_name",hint="text",x=0,y=15,width=8,height=1,value="模板"}
+}
+
+dialog_import_config={
+	{class="label",label="翻译的效果",x=0,y=0,width=20,height=1},
+	{class="textbox",name="trans_prefix",hint="tags",x=0,y=1,width=20,height=3},
+	{class="label",label="原文的效果",x=0,y=4,width=20,height=1},
+	{class="textbox",name="src_prefix",hint="tags",x=0,y=5,width=20,height=3}
+}
+
+function tpl_file_load(filename)
+then
+	local tpl_table={}
+	local tpl_file=io.open(filename,r)
+	
+	for line in tpl_file:lines()
+	do
+		
+	end
+	
+	tpl_file:close()
+	return tpl_table
+end
+
+function export(subtitles,selected_lines)
+	local tpl_sub=subtitles[selected_lines[0]]
+	local tpl_style=bakery_get_style_by_name(tpl_sub.style)
+	
+	bakery_set_control_value_by_name(dialog_export_config,"style",tpl_sub.style)
+	bakery_set_control_value_by_name(dialog_export_config,"layer",tpl_sub.layer)
+	bakery_set_control_value_by_name(dialog_export_config,"text",tpl_sub.text)
+	
+	result = bakery_simple_dialog_ok_quit(dialog_export_config)
+	
+	file_name = aegisub.dialog.save("保存模板", "template.tpl", "", wildcards, true)
+	if file_name == nil
+	then
+		aegisub.cancel()
+	end
+	
+	
+end
+
+function import(subtitles)
+end
+
+function entry(subtitles,selected_lines)
+	result = bakery_simple_dialog_ok_quit(dialog_mode)
+	
+	if result.mode == "生成模板"
+	then
+		export(subtitles,selected_lines)
+	else
+		import()
+	end
+end
+
+function bilingual_tpl_macro(subtitles, selected_lines, active_line)
+	entry(subtitles,selected_lines)
+	aegisub.set_undo_point(script_name)
+end
+
+function bilingual_tpl_filter(subtitles, config)
+	entry(subtitles)
+end
+
+aegisub.register_macro(script_name, script_description, bilingual_tpl_macro)
+aegisub.register_filter(script_name, script_description, 0, bilingual_tpl_filter)
